23ed5331b5023333560468a31463c4836db3d003
sebasorozco101596 <sebasorozcob@gmail.com>
2021-02-03 14:52:53 -0500

    Adding the DataBinding with the Adapter of the recipes_row_layout.xml

--- app/src/main/java/com/sebasorozcob/www/foodtil/MainViewModel.kt
+++ app/src/main/java/com/sebasorozcob/www/foodtil/MainViewModel.kt
@@ -11,7 +11,6 @@ import androidx.lifecycle.viewModelScope
 import com.sebasorozcob.www.foodtil.data.network.Repository
 import com.sebasorozcob.www.foodtil.models.FoodRecipe
 import com.sebasorozcob.www.foodtil.util.NetworkResult
-import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import retrofit2.Response
 
---
+++ app/src/main/java/com/sebasorozcob/www/foodtil/ui/adapters/RecipesAdapter.kt
@@ -0,0 +1,51 @@
+package com.sebasorozcob.www.foodtil.ui.adapters
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.RecyclerView
+import com.sebasorozcob.www.foodtil.databinding.RecipesRowLayoutBinding
+import com.sebasorozcob.www.foodtil.models.FoodRecipe
+import com.sebasorozcob.www.foodtil.models.Result
+import com.sebasorozcob.www.foodtil.util.RecipesDiffUtil
+
+class RecipesAdapter : RecyclerView.Adapter<RecipesAdapter.MyViewHolder>() {
+
+    private var recipe = emptyList<Result>()
+
+    class MyViewHolder(private val binding: RecipesRowLayoutBinding) :
+        RecyclerView.ViewHolder(binding.root) {
+        fun bind(result: Result) {
+            binding.result = result
+            binding.executePendingBindings() // This function evaluates the pending bindings, updating any viewers that have expressions
+        }
+
+        companion object {
+            fun from(parent: ViewGroup): MyViewHolder {
+                val layoutInflater = LayoutInflater.from(parent.context)
+                val binding = RecipesRowLayoutBinding.inflate(layoutInflater, parent, false)
+                return MyViewHolder(binding)
+            }
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        return MyViewHolder.from(parent)
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val currentResult = recipe[position]
+        holder.bind(currentResult)
+    }
+
+    override fun getItemCount(): Int {
+        return recipe.size
+    }
+
+    fun setData(newData: FoodRecipe) {
+        val recipesDiffUtil = RecipesDiffUtil(recipe, newData.results)
+        val diffUtilResult = DiffUtil.calculateDiff(recipesDiffUtil)
+        recipe = newData.results
+        diffUtilResult.dispatchUpdatesTo(this)
+    }
+}
